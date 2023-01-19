9cdf1139aab8b3366852fc7c0c93a6e23a8a0e16
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2023-01-19 14:53:38 +0530

    create sign in, sign up and forgot password pages and oauth component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,52 @@
 import React from "react";
-import { useLocation,useNavigate } from "react-router";
-   
+import { useLocation, useNavigate } from "react-router";
 
 function Header() {
-    const location = useLocation()
-    const navigate = useNavigate()
-    function pathMatchRoute(route){
-        if(route === location.pathname){
-            return true
-        }
+  const location = useLocation();
+  const navigate = useNavigate();
+  function pathMatchRoute(route,route2) {
+    if (route === location.pathname || route2 === location.pathname) {
+      return true;
     }
+  }
   return (
     <div className="bg-white border-b shadow-sm sticky top-0 z-50">
       <div className="header flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
-         <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="logo" className="h-5 cursor-pointer" onClick={()=>navigate("/")} 
-         />  
+          <img
+            src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+            alt="logo"
+            className="h-5 cursor-pointer"
+            onClick={() => navigate("/")}
+          />
         </div>
         <div>
-            <ul className="flex space-x-10">
-                <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/") &&"text-black border-b-red-500"}`}onClick={()=>navigate("/home")} >Home</li>
-                <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/offers") &&"text-black border-b-red-500"}`}onClick={()=>navigate("/offers")} >Offers</li>
-                <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/sign-in") &&"text-black border-b-red-500"}`}onClick={()=>navigate("/sign-in")} >Sign In</li>
-            </ul>
+          <ul className="flex space-x-10">
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/","/home") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/home")}
+            >
+              Home
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/offers") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/offers")}
+            >
+              Offers
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/sign-in") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/sign-in")}
+            >
+              Sign In
+            </li>
+          </ul>
         </div>
       </div>
     </div>
