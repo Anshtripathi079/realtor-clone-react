6143df2c93128264755251f8a15610f87bea861b
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2023-02-28 19:29:42 +0530

    create the spinner component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,7 @@
-import React from "react";
 import { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router-dom";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-
-function Header() {
+export default function Header() {
   const [pageState, setPageState] = useState("Sign in");
   const location = useLocation();
   const navigate = useNavigate();
@@ -18,13 +16,13 @@ function Header() {
     });
   }, [auth]);
   function pathMatchRoute(route) {
-    if (route === location.pathname){
+    if (route === location.pathname) {
       return true;
     }
   }
   return (
-    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
-      <div className="header flex justify-between items-center px-3 max-w-6xl mx-auto">
+    <div className="bg-white border-b shadow-sm sticky top-0 z-40">
+      <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
           <img
             src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
@@ -39,7 +37,7 @@ function Header() {
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
                 pathMatchRoute("/") && "text-black border-b-red-500"
               }`}
-              onClick={() => navigate("/home")}
+              onClick={() => navigate("/")}
             >
               Home
             </li>
@@ -62,9 +60,7 @@ function Header() {
             </li>
           </ul>
         </div>
-      </div>
+      </header>
     </div>
   );
 }
-
-export default Header;
